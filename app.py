9abf05ee2f4be772f50594797add56120c261c81
Nick Kildall <nkildall@gmail.com>
2022-02-03 13:33:36 -0800

    Created a test form, changed some styling

--- app.py
+++ app.py
@@ -1,13 +1,34 @@
 from flask import Flask, render_template
-
+from flask_wtf import FlaskForm
+from wtforms import StringField, SubmitField
+from wtforms.validators import DataRequired
 
 # Creating Flask instance
 app = Flask(__name__)
+app.config['SECRET_KEY'] = "Super Secret Key... I should hide this later"
+
+### Create Form Class
+class NameForm(FlaskForm):
+    name = StringField("What is your name?", validators=[DataRequired()])
+    submit = SubmitField("Submit")
 
 # Route Decorator
 @app.route('/')
 def index():
     return render_template("index.html")
+
+
+# Form testing page
+@app.route('/name', methods = ['GET', 'POST'])
+def name():
+    name = None
+    nform = NameForm()
+    if nform.validate_on_submit():
+        name = nform.name.data
+        nform.name.data = ""
+    return render_template("name.html", 
+        name = name, 
+        form = nform) 
 
 # Invalid URL
 @app.errorhandler(404)
@@ -18,3 +39,5 @@ def page_not_found(e):
 @app.errorhandler(500)
 def page_not_found(e): 
     return render_template("404.html"), 500
+
+
